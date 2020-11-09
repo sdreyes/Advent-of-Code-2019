720f6c3d843b9940d8139ac5a563ca6004f0a25f
Shelby Reyes <42514855+sdreyes@users.noreply.github.com>
2020-11-09 11:34:31 -0500

    Solve day 2 part 2 puzzle and refactor day 2 part 1 puzzle

--- day2.js
+++ day2.js
@@ -42,7 +42,7 @@ Here are the initial and final states of a few more small programs:
 1,0,0,0,99 becomes 2,0,0,0,99 (1 + 1 = 2).
 2,3,0,3,99 becomes 2,3,0,6,99 (3 * 2 = 6).
 2,4,4,5,99,0 becomes 2,4,4,5,99,9801 (99 * 99 = 9801).
-1,1,1,4,99,5,6,0,99 becomes 30,1,1,4,2,5,6,0,99.
+1,1,1,4,2,5,6,0,99 becomes 30,1,1,4,2,5,6,0,99.
 Once you have a working computer, the first step is to restore the gravity assist program (your puzzle input) to the "1202 program alarm" state it had just before the last computer caught fire. To do this, before running the program, replace position 1 with the value 12 and replace position 2 with the value 2. What value is left at position 0 after the program halts?
 */
 
@@ -59,41 +59,75 @@ const getIntcode = function(filepath) {
   return readFile(filepath).split(",").map(code => parseInt(code));
 };
 
-const performOpcodeCalculation = function(opcode, pos1, pos2, pos3, intcode) {
+const performOpcodeCalculation = function(opcode, param1, param2, param3, intcode) {
   switch(opcode) {
     case 1:
-      intcode[pos3] = intcode[pos1] + intcode[pos2]
+      intcode[param3] = intcode[param1] + intcode[param2]
       return intcode;
     case 2:
-      intcode[pos3] = intcode[pos1] * intcode[pos2]
+      intcode[param3] = intcode[param1] * intcode[param2]
       return intcode;
     default:
       throw "Something went wrong.";
   }
 };
 
-const restoreGravityAssist = function(intcode) {
-  intcode[1] = 12;
-  intcode[2] = 2;
+const restoreGravityAssist = function(intcode, noun, verb) {
+  intcode[1] = noun;
+  intcode[2] = verb;
   return intcode;
 };
 
-const runIntcode = function(intcodeFilepath) {
-  let intcode = restoreGravityAssist(getIntcode(intcodeFilepath));
+const runIntcode = function(intcodeFilepath, noun, verb) {
+  let intcode = restoreGravityAssist(getIntcode(intcodeFilepath), noun, verb);
   let opcode = intcode[0];
-  let pos1 = intcode[1];
-  let pos2 = intcode[2];
-  let pos3 = intcode[3];
+  let param1 = intcode[1];
+  let param2 = intcode[2];
+  let param3 = intcode[3];
   let i = 0
   while (opcode != endProgramOpcode) {
-    intcode = performOpcodeCalculation(opcode, pos1, pos2, pos3, intcode);
+    intcode = performOpcodeCalculation(opcode, param1, param2, param3, intcode);
     i = i + 4;
     opcode = intcode[i];
-    pos1 = intcode[i+1];
-    pos2 = intcode[i+2];
-    pos3 = intcode[i+3];
+    param1 = intcode[i+1];
+    param2 = intcode[i+2];
+    param3 = intcode[i+3];
   }
-  return intcode;
+  return intcode[0];
 };
 
-console.log(`Part 1 solution: ${runIntcode("day2-input.txt")}`);
+console.log(`Part 1 solution: ${runIntcode("day2-input.txt", 12, 2)}`);
+
+/*
+--- Part Two ---
+"Good, the new computer seems to be working correctly! Keep it nearby during this mission - you'll probably use it again. Real Intcode computers support many more features than your new one, but we'll let you know what they are as you need them."
+
+"However, your current priority should be to complete your gravity assist around the Moon. For this mission to succeed, we should settle on some terminology for the parts you've already built."
+
+Intcode programs are given as a list of integers; these values are used as the initial state for the computer's memory. When you run an Intcode program, make sure to start by initializing memory to the program's values. A position in memory is called an address (for example, the first value in memory is at "address 0").
+
+Opcodes (like 1, 2, or 99) mark the beginning of an instruction. The values used immediately after an opcode, if any, are called the instruction's parameters. For example, in the instruction 1,2,3,4, 1 is the opcode; 2, 3, and 4 are the parameters. The instruction 99 contains only an opcode and has no parameters.
+
+The address of the current instruction is called the instruction pointer; it starts at 0. After an instruction finishes, the instruction pointer increases by the number of values in the instruction; until you add more instructions to the computer, this is always 4 (1 opcode + 3 parameters) for the add and multiply instructions. (The halt instruction would increase the instruction pointer by 1, but it halts the program instead.)
+
+"With terminology out of the way, we're ready to proceed. To complete the gravity assist, you need to determine what pair of inputs produces the output 19690720."
+
+The inputs should still be provided to the program by replacing the values at addresses 1 and 2, just like before. In this program, the value placed in address 1 is called the noun, and the value placed in address 2 is called the verb. Each of the two input values will be between 0 and 99, inclusive.
+
+Once the program has halted, its output is available at address 0, also just like before. Each time you try a pair of inputs, make sure you first reset the computer's memory to the values in the program (your puzzle input) - in other words, don't reuse memory from a previous attempt.
+
+Find the input noun and verb that cause the program to produce the output 19690720. What is 100 * noun + verb? (For example, if noun=12 and verb=2, the answer would be 1202.)
+*/
+
+const completeGravityAssist = function(filepath, gravityAssistDesiredOutput) {
+  let output;
+  for (noun = 0; noun <= 99; noun++) {
+    for (verb = 0; verb <= 99; verb++) {
+      output = runIntcode(filepath, noun, verb);
+      if (output === gravityAssistDesiredOutput) return (100 * noun + verb);
+    }
+  }
+  return `${gravityAssistDesiredOutput} is not attainable.`
+}
+
+console.log(`Part 2 solution: ${completeGravityAssist("day2-input.txt", 19690720)}`);
