c1dcc523748d8a5e2cfb4544c205623d0bfbc078
sdreyes <sheshechan90@gmail.com>
2020-11-10 17:15:48 -0500

    Solve day 4 part 1

--- day4.js
+++ day4.js
@@ -17,3 +17,58 @@ How many different passwords within the range given in your puzzle input meet th
 
 Your puzzle input is 124075-580769.
 */
+
+const input = "124075-580769";
+
+// Gets the digit in the 'i'th spot of the full num
+const getDigit = (num, i) => {
+  return Math.floor(Math.abs(num) / Math.pow(10, i)) % 10;
+};
+
+// Counts the number of digits in num
+const digitCount = num => {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+};
+
+const isSixDigits = function(num) {
+  return digitCount(num) === 6;
+};
+
+const hasSameAdjacentDigits = function(num) {
+  for (let i = 1; i < 6; i++) {
+    if (getDigit(num, i) === getDigit(num, i-1)) return true;
+  };
+  return false;
+};
+
+const digitsIncrease = function(num) {
+  for (let i = 5; i > 0; i--) {
+    // If the digit is smaller than the one preceding it, return false
+    if (getDigit(num, i-1) < getDigit(num, i)) return false;
+  };
+  return true;
+};
+
+let getNumValidPasswords = function(input) {
+  let numValidPasswords = 0;
+  let range = input.split("-");
+  let minRange = parseInt(range[0]);
+  let maxRange = parseInt(range[1]);
+  // console.log(minRange, maxRange);
+  for (let i = minRange; i < maxRange; i++) {
+    if (isSixDigits(i)) {
+      if (hasSameAdjacentDigits(i) && digitsIncrease(i)) numValidPasswords++;
+    };
+  };
+  return numValidPasswords;
+}
+
+console.log(isSixDigits(123456)); //true
+console.log(isSixDigits(2)); //false
+console.log(hasSameAdjacentDigits(11)); //true
+console.log(hasSameAdjacentDigits(123456)); //false
+console.log(digitsIncrease(123456)); //true
+console.log(digitsIncrease(111211)); //false
+console.log(getNumValidPasswords(input));
+
