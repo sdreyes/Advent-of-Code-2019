c445feaa2dae64c0193fc0896142f7c7c3633fe0
Shelby Reyes <42514855+sdreyes@users.noreply.github.com>
2020-11-12 17:12:15 -0500

    Solve day 6 part 2 and add comments explaining the solutions

--- day6.js
+++ day6.js
@@ -65,6 +65,7 @@ const getOrbitData = (filepath) => {
 
 const parseOrbitData = (orbitData) => {
   let obj = {};
+  // Create a list of key value pairs, where the key is the orbiter, and the value is the object being orbited
   for (let i = 0; i < orbitData.length; i++) {
     const orbitee = orbitData[i].split(")")[0];
     const orbiter = orbitData[i].split(")")[1];
@@ -74,13 +75,15 @@ const parseOrbitData = (orbitData) => {
 };
 
 const countOrbits = (i, orbitData) => {
+  // If the object exists as an orbiter, add 1 and then recursively check if its value exists as an orbiter. Once it finds a value that doesn't exist as an orbiter, it returns 0 and the call stack ends
   if (orbitData[i]) return 1 + countOrbits(orbitData[i], orbitData)
   return 0;
 };
 
 const checkSum = filepath => {
-  let orbitData = getOrbitData("day6-input.txt");
+  let orbitData = getOrbitData(filepath);
   let count = 0;
+  // Because every object is in orbit around exactly one other object, the orbiters are all unique (keys)
   for (const i in orbitData) {
     count += countOrbits(i, orbitData);
   }
@@ -88,3 +91,90 @@ const checkSum = filepath => {
 };
 
 console.log(checkSum("day6-input.txt"));
+
+/*
+--- Part Two ---
+Now, you just need to figure out how many orbital transfers you (YOU) need to take to get to Santa (SAN).
+
+You start at the object YOU are orbiting; your destination is the object SAN is orbiting. An orbital transfer lets you move from any object to an object orbiting or orbited by that object.
+
+For example, suppose you have the following map:
+
+COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN
+Visually, the above map of orbits looks like this:
+
+                          YOU
+                         /
+        G - H       J - K - L
+       /           /
+COM - B - C - D - E - F
+               \
+                I - SAN
+In this example, YOU are in orbit around K, and SAN is in orbit around I. To move from K to I, a minimum of 4 orbital transfers are required:
+
+K to J
+J to E
+E to D
+D to I
+Afterward, the map of orbits looks like this:
+
+        G - H       J - K - L
+       /           /
+COM - B - C - D - E - F
+               \
+                I - SAN
+                 \
+                  YOU
+What is the minimum number of orbital transfers required to move from the object YOU are orbiting to the object SAN is orbiting? (Between the objects they are orbiting - not between YOU and SAN.)
+*/
+
+const getPath = (i, orbitData, pathArr) => {
+  // Capture a list of every orbit jumped to to reach the orbiter specified
+  if (orbitData[i]) {
+    // If the orbiter exists in the list, push the orbitee to the array
+    pathArr.push(orbitData[i]);
+    // Then find out what the orbitee is orbiting
+    getPath(orbitData[i], orbitData, pathArr)
+  }
+  return pathArr;
+};
+
+const contains = (arr, val) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === val) return true;
+  }
+  return false;
+};
+
+const numOrbitsToSanta = filepath => {
+  let orbitData = getOrbitData(filepath);
+  // Capture the orbit path to get to SAN from COM
+  let santaLoc = getPath("SAN", orbitData, []);
+  // Capture the orbit path to get to YOU from COM
+  let youLoc = getPath("YOU", orbitData, []);
+  // Counter to capture how many orbital transfers are required
+  let orbitsToSanta = 0;
+  // Count how many orbits to SantaLoc aren't in common with youLoc
+  for (let i = 0; i < santaLoc.length; i++) {
+    if (!contains(youLoc, santaLoc[i])) orbitsToSanta++;
+  }
+  // Count how many orbits to YouLoc aren't in common with SantaLoc
+  for (let j = 0; j < santaLoc.length; j++) {
+    if (!contains(santaLoc, youLoc[j])) orbitsToSanta++;
+  }
+  return orbitsToSanta;
+}
+
+console.log(numOrbitsToSanta("day6-input.txt"));
